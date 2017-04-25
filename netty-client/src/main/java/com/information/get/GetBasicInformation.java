0af21c9788be16261f78cc5e582a80ab6b6fabaf
DONGHYUK <ehdgur920907@gmail.com>
2017-04-25 11:49:45 +0900

    remove sigar lib, add oshi

--- netty-client/src/main/java/com/information/get/GetBasicInformation.java
+++ netty-client/src/main/java/com/information/get/GetBasicInformation.java
@@ -2,17 +2,17 @@ package com.information.get;
 
 import java.io.File;
 
-import org.hyperic.sigar.Mem;
-import org.hyperic.sigar.Sigar;
-
 import com.information.model.BasicInformation;
+
+import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
 
 public class GetBasicInformation implements Get {
 	@Override
 	public BasicInformation execute() {
 		BasicInformation basicInformation = new BasicInformation();
-		Sigar sigar = new Sigar();
-		Mem mem = null;
+		SystemInfo si = new SystemInfo();
+		HardwareAbstractionLayer hal = si.getHardware();
 		
 		File[] roots = File.listRoots();
 
@@ -22,17 +22,11 @@ public class GetBasicInformation implements Get {
 			basicInformation.setUsedDisk(String.valueOf(String.format("%.2f", root.getUsableSpace() / Math.pow(1024, 3))));
 			basicInformation.setFreeDisk(String.valueOf(String.format("%.2f", (root.getTotalSpace() / Math.pow(1024, 3)) - (root.getUsableSpace() / Math.pow(1024, 3)))));
 		}
-
-		try {
-			mem = sigar.getMem();
-
-			/* KB -> GB */
-			basicInformation.setTotalMemory(String.valueOf(String.format("%.2f", Math.round(((double) mem.getTotal() / 1000000000) * 100) / 100.0)));
-			basicInformation.setUsedMemory(String.valueOf(String.format("%.2f", Math.round(((double) mem.getUsed() / 1000000000) * 100) / 100.0)));
-			basicInformation.setFreeMemory(String.valueOf(String.format("%.2f", Math.round(((double) mem.getFree() / 1000000000) * 100) / 100.0)));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
+		basicInformation.setTotalMemory(String.valueOf(String.format("%.2f", (hal.getMemory().getTotal() * 0.00000001))));
+		basicInformation.setUsedMemory(String.valueOf(String.format("%.2f", (hal.getMemory().getAvailable() * 0.00000001))));
+		basicInformation.setFreeMemory(String.valueOf(String.format("%.2f", (hal.getMemory().getTotal() * 0.00000001) - (hal.getMemory().getAvailable() * 0.00000001))));
+	
 		return basicInformation;
 	}
 }
