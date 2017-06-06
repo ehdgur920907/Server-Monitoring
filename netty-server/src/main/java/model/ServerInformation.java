3998c53c43ae257aea9e31840ff652ff4e2e3d05
DONGHYUK <ehdgur920907@gmail.com>
2017-06-06 12:07:15 +0900

    add netty-server

---
+++ netty-server/src/main/java/model/ServerInformation.java
@@ -0,0 +1,50 @@
+package model;
+
+public class ServerInformation {
+	private String id;
+	private String osName;
+	private String ipAddress;
+	private String hostName;
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getOsName() {
+		return osName;
+	}
+
+	public void setOsName(String osName) {
+		this.osName = osName;
+	}
+
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+}
